4d4dd723996279fbe5025f00e33a34dc545069f9
Fonbod Wilson <160759769+Fonbod1@users.noreply.github.com>
2025-09-05 16:12:03 +0100

    nouveau-clt-frs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,13 @@ import { PageLoginComponent } from './pages/page-login/page-login.component';
 import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
 import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
 import { PageStatistiquesComponent } from './pages/page-statistiques/page-statistiques.component';
+import { PageArticleComponent } from './pages/articles/page-article/page-article.component';
+import { NouvelArticleComponent } from './pages/articles/nouvel-article/nouvel-article.component';
+import { PageMvtstkComponent } from './pages/mvtstk/page-mvtstk/page-mvtstk.component';
+import { PageClientComponent } from './pages/client/page-client/page-client.component';
+import { PageFournisseurComponent } from './pages/fournisseur/page-fournisseur/page-fournisseur.component';
+import { NouveauCltFrsComponent } from './composants/nouveau-clt-frs/nouveau-clt-frs.component';
+
 
 const routes: Routes = [
        { path: 'login', component: PageLoginComponent },
@@ -16,8 +23,36 @@ const routes: Routes = [
                 {
                    path:  'statistiques',
                    component: PageStatistiquesComponent
-                  }
+                  },
 
+                {
+                   path: 'articles',
+                   component: PageArticleComponent
+                  },
+                 {
+                   path: 'nouvelarticle',
+                   component: NouvelArticleComponent
+                  },
+                {
+                  path: 'mvtstk',
+                  component: PageMvtstkComponent
+                },
+              {
+                path: 'clients',
+                component: PageClientComponent
+               },
+                {
+                  path: 'nouveauclient',
+                  component: NouveauCltFrsComponent
+                 },
+               {
+                 path: 'fournisseurs',
+                 component: PageFournisseurComponent
+                },
+              {
+                path: 'nouveaufournisseur',
+                component:  NouveauCltFrsComponent
+                }
 
               ]
             }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,19 @@ import { PageLoginComponent } from './pages/page-login/page-login.component';
 import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
 import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
 import { PageStatistiquesComponent } from './pages/page-statistiques/page-statistiques.component';
+import { MenuComponent } from './composants/menu/menu.component';
+import { HeaderComponent } from './composants/header/header.component';
+import { PageArticleComponent } from './pages/articles/page-article/page-article.component';
+import { DetailArticleComponent } from './composants/detail-article/detail-article.component';
+import { PaginationComponent } from './composants/pagination/pagination.component';
+import { NouvelArticleComponent } from './pages/articles/nouvel-article/nouvel-article.component';
+import { PageMvtstkComponent } from './pages/mvtstk/page-mvtstk/page-mvtstk.component';
+import { DetailMvtStkArticleComponent } from './composants/detail-mvt-stk-article/detail-mvt-stk-article.component';
+import { DetailCltFrsComponent } from './composants/detail-clt-frs/detail-clt-frs.component';
+import { PageClientComponent } from './pages/client/page-client/page-client.component';
+import { PageFournisseurComponent } from './pages/fournisseur/page-fournisseur/page-fournisseur.component';
+import { ButtonActionComponent } from './composants/button-action/button-action/button-action.component';
+import { NouveauCltFrsComponent } from './composants/nouveau-clt-frs/nouveau-clt-frs.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +27,20 @@ import { PageStatistiquesComponent } from './pages/page-statistiques/page-statis
     PageLoginComponent,
     PageInscriptionComponent,
    PageDashboardComponent,
-   PageStatistiquesComponent
+   PageStatistiquesComponent,
+   MenuComponent,
+   HeaderComponent,
+   PageArticleComponent,
+   DetailArticleComponent,
+   PaginationComponent,
+   NouvelArticleComponent,
+   PageMvtstkComponent,
+   DetailMvtStkArticleComponent,
+   DetailCltFrsComponent,
+   PageClientComponent,
+   PageFournisseurComponent,
+   ButtonActionComponent,
+   NouveauCltFrsComponent
   ],
   imports: [
     BrowserModule,
