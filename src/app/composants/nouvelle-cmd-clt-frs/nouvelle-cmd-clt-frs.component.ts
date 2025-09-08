49944f67e959551b5acde322ea1519e0f857329d
Fonbod Wilson <160759769+Fonbod1@users.noreply.github.com>
2025-09-08 19:18:00 +0100

    customer/supplier cmd interface

---
+++ src/app/composants/nouvelle-cmd-clt-frs/nouvelle-cmd-clt-frs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nouvelle-cmd-clt-frs',
+  templateUrl: './nouvelle-cmd-clt-frs.component.html',
+  styleUrls: ['./nouvelle-cmd-clt-frs.component.scss']
+})
+export class NouvelleCmdCltFrsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
