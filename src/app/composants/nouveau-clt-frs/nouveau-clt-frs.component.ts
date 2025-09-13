a90fe4fd6af51a5b89ab7f88ca369b396bd5c1a1
Fonbod Wilson <160759769+Fonbod1@users.noreply.github.com>
2025-09-13 16:40:34 +0100

    Change Pasword

--- src/app/composants/nouveau-clt-frs/nouveau-clt-frs.component.ts
+++ src/app/composants/nouveau-clt-frs/nouveau-clt-frs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Routes} from "@angular/router";
 
 @Component({
   selector: 'app-nouveau-clt-frs',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NouveauCltFrsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+
+  ) { }
 
   ngOnInit(): void {
   }
